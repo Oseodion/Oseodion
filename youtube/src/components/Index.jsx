72af99c2b8188cab807fbf15c77b82f24b6d48cf
Oseodion <102174239+Oseodion@users.noreply.github.com>
2024-03-26 12:51:45 +0100

    Delete youtube directory

--- youtube/src/components/Index.jsx
+++
@@ -1,9 +0,0 @@
-export {default as Navbar} from './Navbar';
-export {default as Feed} from './Feed';
-export {default as ChannelDetails} from './ChannelDetails';
-export {default as SearchFeed} from './SearchFeed';
-export {default as VideoDetails} from './VideoDetails';
-export {default as Videos} from './Videos';
-export {default as SideBar} from './Sidebar';
-export {default as VideoCard} from './VideoCard';
-export {default as ChannelCard} from './ChannelCard';
